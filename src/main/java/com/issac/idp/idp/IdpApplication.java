d260c5e7e53f86ed7825730ba8316c1c79745a31
issac <issac.rajan@gmail.com>
2022-12-02 15:25:43 +0530

    init

---
+++ src/main/java/com/issac/idp/idp/IdpApplication.java
@@ -0,0 +1,13 @@
+package com.issac.idp.idp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issac/idp/idp/IdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issac.idp.idp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

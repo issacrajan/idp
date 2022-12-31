133696e6fd02e28c2d4163c65a2ff0ea06bce21c
issac <issac.rajan@gmail.com>
2022-12-31 10:34:10 +0530

    test2 v1

---
+++ src/main/java/com/issac/idp/test/Test2.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2022 issac.rajan@gmail.com. All rights reserved.
+ */
+package com.issac.idp.test;
+
+/**
+ * @author issac
+ *
+ */
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("one");
+	}
+}
